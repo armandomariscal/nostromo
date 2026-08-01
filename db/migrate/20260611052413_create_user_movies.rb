8143a174f5e843bba87a3ec3b248c05a5322e986
Armando Mariscal <armando.marisscal@gmail.com>
2026-07-31 23:17:31 -0600

    feat(db): add initial database schema and user migrations

---
+++ db/migrate/20260611052413_create_user_movies.rb
@@ -0,0 +1,11 @@
+class CreateUserMovies < ActiveRecord::Migration[8.1]
+  def change
+    create_table :user_movies do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :movie, null: false, foreign_key: true
+      t.boolean :viewed
+
+      t.timestamps
+    end
+  end
+end
