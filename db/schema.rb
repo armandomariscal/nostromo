ffb746d947e1306b4bd065663215d1df29280bea
Armando Mariscal <armando.marisscal@gmail.com>
2026-08-03 23:26:59 -0600

    chore(schema): add crew_members table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_11_052413) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_04_051834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "crew_members", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
