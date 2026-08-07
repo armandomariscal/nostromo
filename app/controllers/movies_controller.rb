1f4aa63f2117238206d4e5a6e8199500ae18be06
Armando Mariscal <armando.marisscal@gmail.com>
2026-08-06 22:56:49 -0600

    refactor(movies): extract shared controller setup

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,24 +1,37 @@
 class MoviesController < ApplicationController
+  before_action :set_current_user
+  before_action :set_movie, only: :show
+  before_action :set_progress
+
   def index
-    @current_user = User.first
     @movies = Movie.includes(:user_movies).order(:year)
-
-    @total_movies = @movies.count
-
-    @viewed_movies = @current_user.user_movies
-                                .where(viewed: true)
-                                .count
-
-    @progress_percentage =
-      (@viewed_movies.to_f / @total_movies * 100).round
   end
 
   def show
-    @current_user = User.first
-    @movie = Movie.find(params[:id])
+  end
 
+  private
+
+  def set_current_user
+    @current_user = User.first
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def set_progress
     @total_movies = Movie.count
-    @viewed_movies = @current_user.user_movies.where(viewed: true).count
-    @progress_percentage = (@viewed_movies.to_f / @total_movies * 100).round
+
+    @viewed_movies = @current_user.user_movies
+                                  .where(viewed: true)
+                                  .count
+
+    @progress_percentage =
+      if @total_movies.zero?
+        0
+      else
+        (@viewed_movies.to_f / @total_movies * 100).round
+      end
   end
 end
