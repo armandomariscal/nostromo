d0b008175beaa14463482ce7023d4806f4d14e1c
Armando Mariscal <armando.marisscal@gmail.com>
2026-08-11 23:05:51 -0600

    refactor(reviews): style review form and align navbar controller setup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,47 @@
 class ReviewsController < ApplicationController
+  before_action :set_current_user
+  before_action :set_movie
+  before_action :set_progress, only: [:new, :create]
+
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = Review.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
-
-    @review = @movie.reviews.new(review_params)
-    @review.user = User.first
+    @review = @movie.reviews.build(review_params)
+    @review.user = @current_user
 
     if @review.save
-      redirect_to @movie
+      redirect_to movie_path(@movie), notice: "Review created successfully."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_current_user
+    @current_user = User.first
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def set_progress
+    @total_movies = Movie.count
+
+    @viewed_movies = @current_user.user_movies
+                                  .where(viewed: true)
+                                  .count
+
+    @progress_percentage =
+      if @total_movies.zero?
+        0
+      else
+        (@viewed_movies.to_f / @total_movies * 100).round
+      end
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
